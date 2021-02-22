c6abb766003b1a709b888172be1c593df9e730d4
Ochornma <ochornmapromise@gmail.com>
2021-02-22 12:14:23 +0100

    setup for room database

---
+++ app/src/main/java/com/ivorycloud/testingtutorial/local/data/ShoppingItemDatabase.kt
@@ -0,0 +1,13 @@
+package com.ivorycloud.testingtutorial.local.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [ShoppingItem::class],
+    version = 1
+)
+abstract class ShoppingItemDatabase : RoomDatabase() {
+
+    abstract fun shoppingDao(): ShoppingDao
+}
