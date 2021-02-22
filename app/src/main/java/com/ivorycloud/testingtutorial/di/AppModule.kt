178b97d2045079d013bcda4e5e07408d2642573e
Ochornma <ochornmapromise@gmail.com>
2021-02-22 16:08:52 +0100

    hilt dagger setup

---
+++ app/src/main/java/com/ivorycloud/testingtutorial/di/AppModule.kt
@@ -0,0 +1,58 @@
+package com.ivorycloud.testingtutorial.di
+
+import android.content.Context
+import androidx.room.Room
+import com.ivorycloud.testingtutorial.other.Constants.BASE_URL
+import com.ivorycloud.testingtutorial.other.Constants.DATABASE_NAME
+import com.ivorycloud.testingtutorial.local.data.ShoppingItemDatabase
+import com.ivorycloud.testingtutorial.local.remote.PixabayAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+
+    @Singleton
+    @Provides
+    fun provideShoppingItemDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(context, ShoppingItemDatabase::class.java, DATABASE_NAME).build()
+
+    @Singleton
+    @Provides
+    fun provideShoppingDao(
+        database: ShoppingItemDatabase
+    ) = database.shoppingDao()
+
+    @Singleton
+    @Provides
+    fun providePixabayApi(): PixabayAPI {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PixabayAPI::class.java)
+    }
+
+    /*
+    same as above
+    just for explaniation
+    *    @Singleton
+    @Provides
+    fun providePixabayApi()=
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PixabayAPI::class.java)
+    * */
+}
---
+++ app/src/main/java/com/ivorycloud/testingtutorial/other/Constants.kt
@@ -0,0 +1,7 @@
+package com.ivorycloud.testingtutorial.other
+
+object Constants {
+    //BASE_URL
+    val DATABASE_NAME = "database"
+    val BASE_URL = "https://pixabay.com"
+}
