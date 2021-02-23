7078c69e3b2552ccbfd04f38aaf9cfd28c57d144
Ochornma <ochornmapromise@gmail.com>
2021-02-23 18:15:04 +0100

    testing with hilt

---
+++ app/src/androidTest/java/com/ivorycloud/testingtutorial/di/TestAppModule.kt
@@ -0,0 +1,24 @@
+package com.ivorycloud.testingtutorial.di
+
+import android.content.Context
+import androidx.room.Room
+import com.ivorycloud.testingtutorial.local.data.ShoppingItemDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestAppModule {
+
+    @Provides
+    @Named("test_db")
+    fun provideInMemoryDb(@ApplicationContext context: Context) =
+        Room.inMemoryDatabaseBuilder(context, ShoppingItemDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+}
